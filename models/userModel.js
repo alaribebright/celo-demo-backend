783961fc17bc7677477daae32952430f1dc2c718
alaribebright <bright.alaribe@origne.com>
2022-04-26 13:50:23 +0100

    Add user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,46 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
-  {},
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    countryCode: {
+      type: String,
+      required: true,
+    },
+    celoWalletAddress: {
+      type: String,
+      required: true,
+    },
+    bscWalletAddress: {
+      type: String,
+      required: true,
+    },
+    bankDetails: {
+      name: {
+        type: String,
+        required: true,
+      },
+      accountNumber: {
+        type: String,
+        required: true,
+      },
+      bankName: {
+        type: String,
+        required: true,
+      },
+    },
+    mPesa: {
+      name: String,
+      phoneNumber: String,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
   {
     timestamps: true,
   }
