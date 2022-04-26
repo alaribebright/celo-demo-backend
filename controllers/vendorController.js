4e7ec0a4ceac6baab92c7ac922fd449e18d07e91
alaribebright <bright.alaribe@origne.com>
2022-04-26 13:42:48 +0100

    Fix error message for vendors that are not available

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,29 +1,37 @@
 const Vendor = require("../models/vendorModel");
 
 const getVendor = async (req, res) => {
-  const amount = +req.query.amount;
-
-  const defaultNetwork = process.env.DEFAULT_NETWORK;
-
-  if (defaultNetwork === "celo") {
-    const vendors = await Vendor.find({
-      $and: [{ celoBalance: { $gte: amount } }, { defaultNetwork: "celo" }],
-    });
-
-    if (vendors.length) {
-      return res.status(200).json({
-        status: "success",
-        data: vendors
-      })
-    }
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: "",
-  });
-
   try {
+    const amount = +req.query.amount;
+
+    const defaultNetwork = process.env.DEFAULT_NETWORK;
+
+    if (defaultNetwork === "celo") {
+      const vendors = await Vendor.find({
+        $and: [{ celoBalance: { $gte: amount } }, { defaultNetwork: "celo" }],
+      });
+
+      if (vendors.length) {
+        return res.status(200).json({
+          status: "success",
+          data: vendors,
+        });
+      }
+    } else {
+      const vendors = await Vendor.find({
+        $and: [{ celoBalance: { $gte: amount } }, { defaultNetwork: "bsc" }],
+      });
+
+      if (vendors.length) {
+        return res.status(200).json({
+          status: "success",
+          data: vendors,
+        });
+      }
+    }
+
+    throw new Error("No Available vendors")
+
   } catch (error) {
     res.status(400).json({
       status: "fail",
